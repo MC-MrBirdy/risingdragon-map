b7116c9937902a009b26fc8859d61131da0532fe
MrBirdy <6671799+MC-MrBirdy@users.noreply.github.com>
2021-12-17 07:12:55 +0100

    1.18 update
    Changed the 1.17 maps to have a border to match the 1.18 size. The maps have a slight difference resulting in different coordinate corrections.

--- worlds/worlds-config.js
+++ worlds/worlds-config.js
@@ -2,9 +2,9 @@ var defaultConfig = {
 	"minZoom" : 3,
 	"maxZoom" : 6,
 	"mapResolution" : 1.00000000,
-	"mapExtent" : [-5120.00000000, -5120.00000000, 5120.00000000, 5120.00000000],
+	"mapExtent" : [-5695.00000000, -5695.00000000, 5695.00000000, 5695.00000000],
 	"defaultWorldID" : 0,
-	"coordCor" : 2.0
+	"coordCor" : 2.006
 };
 
 var worldsConfig = [
@@ -13,8 +13,29 @@ var worldsConfig = [
 		"name":"Overworld",
 		"path":"worlds/overworld",
 		"type":"default",
+		"xsize":22500,
+		"ysize":22500,
+		"markers":[
+			{
+				"name":"Homes",
+				"path":"/homes.json",
+				"default":true
+			},
+			{
+				"name":"Warps",
+				"path":"/warps.json",
+				"default":false,
+				"color":"yellow"
+			}]
+	},
+	{
+		"id":9,
+		"name":"Overworld Dec.21 (1.17)",
+		"path":"worlds/overworld-dec21-117",
+		"type":"default",
 		"xsize":20000,
 		"ysize":20000,
+		"coordCor" : 2,
 		"markers":[
 			{
 				"name":"Homes",
@@ -30,11 +51,12 @@ var worldsConfig = [
 	},
 	{
 		"id":10,
-		"name":"Overworld Oct.21",
-		"path":"worlds/overworld-oct21",
+		"name":"Overworld Oct.21 (1.17)",
+		"path":"worlds/overworld-oct21-117",
 		"type":"default",
 		"xsize":20000,
 		"ysize":20000,
+		"coordCor" : 2,
 		"markers":[
 			{
 				"name":"Homes",
